32ea91046ab8523bcace48e830920f13cf79830d
Rafael Falcão <rafael.jardim@casaevideo.com.br>
2023-08-27 17:57:11 -0300

    feat: changed from OpenMeteo to OpenWeather to capture weather metrics; maintained OpenMeteo to get air quality

--- src/main/java/br/com/seeletech/rafiqbot/application/commands/WeatherCommand.java
+++ src/main/java/br/com/seeletech/rafiqbot/application/commands/WeatherCommand.java
@@ -1,9 +1,7 @@
-package br.com.seeletech.rafiqbot.commands;
+package br.com.seeletech.rafiqbot.application.commands;
 
 import br.com.seeletech.rafiqbot.domain.OpenWeatherConditionsEnum;
 import br.com.seeletech.rafiqbot.domain.OpenWeatherDTO;
-import br.com.seeletech.rafiqbot.domain.WeathercodeEnum;
-import br.com.seeletech.rafiqbot.domain.service.LoremPokemonService;
 import br.com.seeletech.rafiqbot.domain.service.OpenMeteoService;
 import br.com.seeletech.rafiqbot.domain.service.OpenWeatherService;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
@@ -16,20 +14,20 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @Component
 public class WeatherCommand implements SlashCommand {
 
-    private final OpenMeteoService openMeteoService;
     private final OpenWeatherService openWeatherService;
-    private final LoremPokemonService loremPokemonService;
+    private final OpenMeteoService openMeteoService;
 
     @Value("${app.bot.ephemeral:}")
     private Boolean isEphemeral;
 
-    public WeatherCommand(OpenMeteoService openMeteoService, OpenWeatherService openWeatherService, LoremPokemonService loremPokemonService) {
-        this.openMeteoService = openMeteoService;
+    public WeatherCommand(OpenWeatherService openWeatherService, OpenMeteoService openMeteoService) {
         this.openWeatherService = openWeatherService;
-        this.loremPokemonService = loremPokemonService;
+        this.openMeteoService = openMeteoService;
     }
 
     @Override
@@ -44,7 +42,12 @@ public class WeatherCommand implements SlashCommand {
                 event.getOption("city_name")
                         .flatMap(ApplicationCommandInteractionOption::getValue)
                         .map(ApplicationCommandInteractionOptionValue::asString)
-                        .get());
+                        .orElseThrow(() -> new Exception("city name invalid or null")));
+
+        Map<String,String> airQuality = this.openMeteoService.getAirQuality(
+                String.valueOf(weather.latitude()),
+                String.valueOf(weather.longitude())
+        );
 
         String weatherImage = this.openWeatherService.buildIconUrl(weather.weatherIcon());
 
@@ -53,7 +56,7 @@ public class WeatherCommand implements SlashCommand {
 
         if (weather.feels() < 10.0) {
             color = Color.BLUE;
-            image = "https://external-preview.redd.it/T7w78aEU-vj1kVjIlbD5FGiu_NpEuIU0qRNE9VWDT78.jpg";
+            image = "https://www.digitalmomblog.com/wp-content/uploads/2022/10/funny-cold-weather-memes.jpeg";
         } else if (weather.feels() < 20.0) {
             color = Color.GREEN;
             image = "https://imgix.bustle.com/uploads/image/2023/7/20/14dfabae-fe90-4277-ad9e-a95344137690-poke-sleep.jpg?w=1200&h=630&fit=crop&crop=faces&fm=jpg";
@@ -73,8 +76,8 @@ public class WeatherCommand implements SlashCommand {
                 .addField("Temp", String.valueOf(weather.temp()).concat(" °C"), true)
                 .addField("Feels", String.valueOf(weather.feels()).concat(" °C"), true)
                 .addField("Weather", StringUtils.capitalize(OpenWeatherConditionsEnum.valueOfId(weather.weatherId()).condition), false)
-//                .addField("CO", String.valueOf(forecast.getCarbon_monoxide()).concat(" μg/m³"), true)
-//                .addField("SO2", String.valueOf(forecast.getSuphur_dioxide()).concat(" μg/m³"), true)
+                .addField("CO", String.valueOf(airQuality.get("co")).concat(" μg/m³"), true)
+                .addField("SO2", String.valueOf(airQuality.get("so2")).concat(" μg/m³"), true)
                 .addField("Lat", String.valueOf(weather.latitude()), true)
                 .addField("Long", String.valueOf(weather.longitude()), true)
                 .addField("Wind", String.valueOf(weather.windSpeed()).concat(" kmh"), true)
